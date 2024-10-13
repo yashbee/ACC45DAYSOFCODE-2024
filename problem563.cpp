7c1b64f9eb2ba017353fc8061f5f330c622e0563
yashbee <39982164+yashbee@users.noreply.github.com>
2024-10-13 09:54:01 -0700

    Add files via upload
    Problem563

---
+++ problem563.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+
+    while (t--) { 
+        int n;
+        cin >> n; 
+
+        
+        int books = (n * 1000) / 100;
+
+       
+        cout << books << endl;
+    }
+
+    return 0;
+}
